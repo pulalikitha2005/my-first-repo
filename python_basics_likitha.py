84acbac11f3b4c369fee4eff9b336b0e54720699
pula.likitha <vtu28727@veltech.edu.in>
2026-04-03 15:12:59 +0530

    Add files via upload

--- python_basics_likitha.py
+++ python_basics_likitha.py
@@ -3,3 +3,10 @@ age = 20
 height = 5.7
 is_student = True
 print(name, age, height, is_student)
+
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+print("Age in months:", age * 12)
+print("Age in days:", age * 365)
+print("Remainder when age is divided by 7:", age % 7)
+print("Age squared:", age ** 2)
